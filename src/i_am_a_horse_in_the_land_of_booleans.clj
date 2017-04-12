86636fe3d65c710003f813ac07153cfcf3b4c787
Johan Björn <Johan.Bjorn@solita.fi>
2017-04-12 20:42:26 +0300

    ex 1-8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,44 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (cond
+    (= x nil) false
+    (= x false) false
+    :else true
+   ))
 
 (defn abs [x]
-  ":(")
+  (if (> x 0) x (* -1 x)))
+
 
 (defn divides? [divisor n]
-  ":(")
+  (= 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? n 15) "gotcha!"
+    (divides? n 5) "buzz"
+    (divides? n 3) "fizz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+      (empty? x) nil
+       (or (list? x) (vector? x)) (* 2 (count x))
+     :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false))
 
 ; '_______'
